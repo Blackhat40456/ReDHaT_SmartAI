cba2ebfa2307e72202429d54801afe7e0e4d0ab8
zarir94 <shahriarzariracc@gmail.com>
2025-08-01 23:29:24 +0600

    revert back

--- AI/tools.py
+++ AI/tools.py
@@ -12,33 +12,22 @@ async def is_uid_verified(bot: Client, uid: int):
 
 
 async def get_deposit_amount(bot: Client, uid: int):
-    total_dep = 0.00
-    i = 0
-    async for msg in bot.search_messages('@ReDHaT4O4_redep_bot', str(uid)):
-        i += 1
-        try:
-            ck = float(msg.text.strip().split(':')[-1].strip())
-            total_dep += ck
-            if not ck < 5 and i == 1:
-                return ck, False
+    async for msg in bot.search_messages('@ReDHaT4O4_redep_bot', str(uid), limit=1):
+        try: return float(msg.text.strip().split(':')[-1].strip())
         except IndexError: pass
         except TypeError: pass
         except ValueError: pass
-    return total_dep, True
+    return 0.00
 
 
 async def check_uid_status(bot: Client, uid: int):
-    note = "The user can either make a single recent deposit of $5 to get verified, or deposit smaller amounts over time that add up to $8 in total."
     try: int(uid)
     except ValueError: return "UID must be a number. Ask user to give UID"
     except TypeError: return "UID must be a number. Ask user to give UID"
     if not await is_uid_verified(bot, uid):
         return f'UID ({uid}) is not verified. Registration needed.'
-    ds, is_total = await get_deposit_amount(bot, uid)
-    if is_total and ds < 8:
-        return f'UID ({uid}) is not verified because "total" deposit is {ds}$ which is less than 8$. More deposit needed. {note}'
-    if not is_total and ds < 5:
-        return f'UID ({uid}) is not verified because "latest" deposit is {ds}$ which is less than 5$. More deposit needed. {note}'
+    if (ds := await get_deposit_amount(bot, uid)) < 5:
+        return f'UID ({uid}) is not verified because latest deposit is {ds}$ which is less than 5$. More deposit needed.'
     return f'UID ({uid}) is verified and deposit is {ds}$. You are good to go.'
     
 
